8c7f395dd5739f685bd73d5b034c33db28b9ef0d
zalditri10 <zalditrighifari@gmail.com>
2023-09-28 17:17:45 +0700

    menambahkan latihan1.php

---
+++ latihan1.php
@@ -0,0 +1,12 @@
+<?php
+Class Latihan1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalan<h1>";
+        echo "Nama Saya ZaldiTri Ghifari dengan NIM 19220760
+              Saya tinggal di daerah Bekasi 
+              Olahraga yang paling kusukai adalah 
+              Berenang"; 
+    }
+}
